0d053df5df2b3294963006cce299ce9a60c4368a
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-01-24 14:23:49 +0700

    Add animation on image upload and show

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import useFirestore from '../hooks/useFirestore';
+import { motion } from 'framer-motion';
 
 const ImageGrid = ({ setSelectedImg }) => {
   const { docs } = useFirestore('images');
-  console.log(docs);
+  // console.log(docs);
   return (
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div
+          <motion.div
             className="img-wrap"
             key={doc.id}
+            whileHover={{ opacity: 1 }}
+            layout
             onClick={() => setSelectedImg(doc.url)}
           >
-            <img src={doc.url} alt="uploaded picture" />
-          </div>
+            <motion.img
+              src={doc.url}
+              alt="uploaded picture"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
+          </motion.div>
         ))}
     </div>
   );
